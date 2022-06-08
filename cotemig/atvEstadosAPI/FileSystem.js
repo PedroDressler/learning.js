7c5656cf10c2165ef0baa27e4485b07f6ba91c19
Pedro Ávila <pedro2003dressler@gmail.com>
2022-06-08 15:59:45 -0300

    Término exercício 2

--- cotemig/atvEstadosAPI/FileSystem.js
+++ cotemig/atvEstadosAPI/FileSystem.js
@@ -1,18 +1,20 @@
-import { existsSync, promises as fs } from 'fs';
+import { promises as fs } from 'fs';
 
 export async function rFile() {
   var estados = await fs.readFile('./json-all/Estados.json');
   estados = JSON.parse(estados);
-  console.log(estados);
 
   var cidades = await fs.readFile('./json-all/Cidades.json');
   cidades = JSON.parse(cidades);
-  // console.log(cidades);
-  if (!existsSync('./json-uf')) {
-    await fs.mkdir('./json-uf', { recursive: true });
-  }
+  console.log(cidades);
+
+  await fs.mkdir('./json-uf', { recursive: true });
+
   for (var i of estados) {
     const UF = cidades.filter(cidade => cidade.Estado == i.ID);
     await fs.writeFile(`./json-uf/${i.Sigla}.json`, JSON.stringify(UF))
   }
+  
 }
+
+rFile();
--- cotemig/atvEstadosAPI/app.js
+++ cotemig/atvEstadosAPI/app.js
@@ -1,3 +1,16 @@
-import { rFile } from './FileSystem.js'
+import { promises as fs } from 'fs';
 
-rFile();
+async function statesQnt(est) {
+  var uf = est.toUpperCase();
+  var data = await fs.readFile(`./json-uf/${uf}.json`);
+  data = JSON.parse(data);
+  // console.log(data);
+  return `Estado: ${est}.\nQuatidade: ${data.length} cidades.`
+}
+
+async function get() {
+  var total = await statesQnt('MG');
+  console.log(total)
+}
+
+get()
